6fa381ecb1c9e677e578d1799578a72ff463b94e
ENSOO <tomasellolorenzo1@gmail.com>
2019-12-01 23:04:28 +0100

    more work to do...

--- python/ex/lez6/classiPolimini.py
+++ python/ex/lez6/classiPolimini.py
@@ -4,36 +4,121 @@ WIDTH=600
 HEIGHT=400
 BGCOLOR='lightgreen'
 
+#rules are adiacent free sides for every square
+
 class Rule():
-    def __init__(self,elements):
+    def __init__(self,positions):
         #it contains A rects, B poly of dim = 2, C poly of dim = 3 ecc.
         self.values=list()
-        for i in elements:
+        self.positions=positions
+        for i in positions:
             elRule=set()
-            for j in elements:
-                if j.y==i.y-1:
-                    elRule.add(0)
-                if j.x==i.x+1:
-                    elRule.add(1)
-                if j.y==i.y+1:
-                    elRule.add(2)
-                if j.x==i.x-1:
-                    elRule.add(3)
+            if True in [True for j in positions if j.y==i.y-1 and j.x==i.x]:
+                elRule.add(0)
+            if True in [True for j in positions if j.y==i.y and j.x==i.x+1]:
+                elRule.add(1)
+            if True in [True for j in positions if j.y==i.y+1 and j.x==i.x]:
+                elRule.add(2)
+            if True in [True for j in positions if j.y==i.y and j.x==i.x-1]:
+                elRule.add(3)
             self.values.append(tuple({0,1,2,3}-elRule))
 
     #for print() function
     def __str__(self):
-        return '\nRule('+str(self.values)+')'
+        return 'Rule('+str(self.values)+')'
     def __repr__(self):
         return 'Rule('+str(self.values)+')'
+
+    def clone(self):
+        return Rule(self.positions)
     
     def getDimension(self):
         return len(self.values)
 
+    #inverse algorithm to find all positions starting from rules
+    @staticmethod
+    def getPositions(values):
+        values=list(values)
+        start=Position(0,0)
+        lastvalue=None
+        positions=[]
+        remembervalues=[]
+        count={0:0}
+        backupvalues={}
+        deep=0
+        while len(values)>0:
+            count[deep+1]=0
+            found=False
+            for i in range(count[deep],len(values)):
+                count[deep]+=1
+                backupvalues[deep]=list(values)
+                currentvalue=values[i]
+                if lastvalue:
+                    #trying to attach to the up
+                    if 0 not in lastvalue and 2 not in currentvalue:
+                        newposition=Position(lastposition.x,lastposition.y)
+                        newposition.move(0,-1)
+                        if not newposition.inArray(positions):
+                            lastposition=newposition
+                            found=True
+                            break
+                    #trying to attach to the right
+                    if 1 not in lastvalue and 3 not in currentvalue:
+                        newposition=Position(lastposition.x,lastposition.y)
+                        newposition.move(1,0)
+                        if not newposition.inArray(positions):
+                            lastposition=newposition
+                            found=True
+                            break
+                    #trying to attach to the down
+                    if 2 not in lastvalue and 0 not in currentvalue:
+                        newposition=Position(lastposition.x,lastposition.y)
+                        newposition.move(0,1)
+                        if not newposition.inArray(positions):
+                            lastposition=newposition
+                            found=True
+                            break
+                    #trying to attach to the left
+                    if 3 not in lastvalue and 1 not in currentvalue:
+                        newposition=Position(lastposition.x,lastposition.y)
+                        newposition.move(-1,0)
+                        if not newposition.inArray(positions):
+                            lastposition=newposition
+                            found=True
+                            break
+                else:
+                    lastposition=start
+                    found=True
+                    break
+            if not found:
+                #it's not possible to attach this at the moment, let's remove the last one and retry
+                lastposition=positions.pop()
+                lastvalue=remembervalues.pop()
+                if len(positions)<1:
+                    lastposition=None
+                    lastvalue=None
+                deep-=1
+                values=backupvalues[deep]
+            else:
+                remembervalues.append(currentvalue)
+                positions.append(lastposition)
+                values.remove(currentvalue)
+                lastvalue=currentvalue
+                deep+=1
+        minX=900
+        minY=900
+        for el in positions:
+            if el.x<minX:
+                minX=el.x
+            if el.y<minY:
+                minY=el.y
+        for el in positions:
+            el.move(-minX,-minY)
+        return positions
+
     #return first occurrence if position respect rule
-    def find(self,pos):
-        print([pos,self])
-        possibilities=Rule.getPoss(pos)
+    def find(self,freeSides):
+        possibilities=Rule.getPoss(freeSides,possibilities=set())
         for i in possibilities:
             if i in self.values:
                 return i
@@ -55,27 +140,47 @@ class Rule():
         return possibilities
 
 class Position():
+    #it's possible to implement here movement or transformation functions and then call them inside Rect class
     def __init__(self,x,y):
         self.x=x
         self.y=y
 
-class Rect():
-    def __init__(self,rule,position=None,size=12,color='#7A7'):
-        self.x=0#edit
-        self.y=0#edit
+    def move(self,x,y):
+        self.x+=x
+        self.y+=y
 
-        if not position:
-            self.freeSides=rule.values.pop(0)
-        else:
-            self.freeSides=rule.find(position)
-            rule.values.remove(self.freeSides)
-        self.rule=rule
+    #for print() function
+    def __str__(self):
+        return 'Position(x:'+str(self.x)+'; y:'+str(self.y)+')'
+    def __repr__(self):
+        return 'Position(x:'+str(self.x)+'; y:'+str(self.y)+')'
+    
+    def inArray(self,arrayPositions):
+        for i in arrayPositions:
+            if i.x==self.x and i.y==self.y:
+                return True
+        return False
+
+class Rect(Position):
+    def __init__(self,position,father,conditions=None,size=12,color='#7A7'):
+
+        # self.rule=rule.clone()#TODO: implement a valid cloning function, this sucks!
+        # self.rule.values=rule.values
+        # if not conditions:
+        #     #this rect will represent the first condition in a list of rules
+        #     self.freeSides=self.rule.values.pop(0)
+        # else:
+        #     #this rect will represent the first condition that respects the rule
+        #     self.freeSides=self.rule.find(conditions)
+        #     self.rule.values.remove(self.freeSides)
+        self.father=father
         self.size=size
         self.color=color
-    def draw(self,x=0,y=0):
+        super().__init__(position.x*self.size+father.x,position.y*self.size+father.y)
+    def draw(self):
         canvas.create_rectangle(self.x,self.y,self.x+self.size,self.y+self.size,fill=self.color,outline='black',tags=('square'))
-    def getNext(self):
-        return Rect(self.rule,position=self.freeSides,size=self.size,color=self.color)
+    # def getNext(self):
+    #     return Rect(rule=self.rule,conditions=self.freeSides,size=self.size,color=self.color)
 
 
 class Polyomino():
@@ -85,18 +190,19 @@ class Polyomino():
         self.y=y
         self.dim=uniquerule.getDimension()
         self.uniquerule=uniquerule
+        self.positions=Rule.getPositions(self.uniquerule.values)
         self.elements=[]
-        el=Rect(uniquerule)
-        self.elements.append(el)
-        nextEl=el.getNext()
-        while nextEl!=None:
-            self.elements.append(nextEl)
-            nextEl=nextEl.getNext()
+        for i in self.positions:
+            self.elements.append(Rect(i,self))
+    def draw(self):
+        for i in self.elements:
+            i.draw()
 
-        print(self.elements)
-    def draw():
-        for i in self.components:
-            i.draw(x,y)
+    #for print() function
+    def __str__(self):
+        return 'Polyomino('+str(self.elements)+')'
+    def __repr__(self):
+        return 'Polyomino('+str(self.elements)+')'
     
 
 def PolyArea(canvas,x,y):
@@ -108,16 +214,19 @@ tk.title('Polimini')
 canvas=Canvas(tk,width=WIDTH,height=HEIGHT,bg=BGCOLOR)
 canvas.pack()
 tk.update()
-polyarea=PolyArea(canvas,200,10)
+polyarea=PolyArea(canvas,10,10)
 tk.update()
 
 positionArray=[
-    Position(3,0),
     Position(1,0),
     Position(0,0),
-    Position(2,0)
+    Position(-2,0),
+    Position(-1,0)
 ]
-myRule=Rule(positionArray)#Rule([(0, 2, 3), (0, 2), (0, 2), (0, 1, 2)])
-Polyomino(myRule)
+myRule=Rule(positionArray)
+print(myRule)
+polyomino=Polyomino(myRule,20,20)
+polyomino.draw()
+print(polyomino)
 
 tk.mainloop()
